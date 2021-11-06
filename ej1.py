aee8b6381ec6c239d0fd613a58878c7d76f9c761
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2021-11-06 12:19:17 +0100

    haciendo el ejercicio de las cuentas bancarias

---
+++ ej1.py
@@ -0,0 +1,13 @@
+
+class Libro:
+    #definir los atributos
+    #situar los valores asociados
+
+
+    isbn=0       #texto
+    títito=0    #texto
+    año_de_publicación=0    #texto
+    ideioma=0   #texto
+    ejemplares=0    #texto
+
+
---
+++ ej4.py
@@ -0,0 +1,32 @@
+class CuentaBancaria:
+
+    ID=0    #texto
+    nombre_titular=0    #texto
+    fecha_apertura=0    #texto
+    num_cuenta=0    #int
+    saldo=0    #int
+
+
+    def RetirarDinero(num_cuenta, cantidadRetirar):
+
+        # sacar una cantidad de dinero determinada
+        if saldo<cantidadRetirar:
+            return('No puedas sacar tanto dinero de tu cuenta porque no tienes saldo suficiente')
+
+        else:
+            saldoRestante=saldo-cantidadRetirar
+            saldoRestante=str(saldoRestante)
+            return ('cantidad retirada, sus saldo restante es de: '+saldoRestante)
+
+
+    def IngresarDinero(num_cuenta, cantidadIngresar):
+        #método de ingresar dinero, se pasa una cantidad por parámetro y se ingresa en la cuenta
+        saldoTotal=saldo+cantidadIngresar
+        return('Dinero ingresado. Tiene una cantidad total de: '+str(saldoTotal))
+
+
+    def TransferirDinero(num_cuenta1, num_cuenta2, dineroTransferido):
+        RetirarDinero(num_cuenta1, dineroTransferido )
+        IngresarDinero(num_cuenta2, dineroTransferido)
+        return ('Dinero total transferido: '+str(dineroTransferido))
+
